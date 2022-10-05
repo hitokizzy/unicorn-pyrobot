e425c46fcc46ea8e10101c5e75d6e49a7df462e4
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-10-05 17:32:10 +0700

    Update system.py

--- unicorns/modules/system.py
+++ unicorns/modules/system.py
@@ -30,6 +30,7 @@ logo = "https://telegra.ph/file/4b788cea5c1413f9496a3.png"
 # ================= CONSTANT =================
 CUSTOM_MSG = ALIVE_MSG or f"`{get_translation('unicornAlive')}`"
 UNICORN_ALV = (
+f"{logo}"
 f"**[Unicorn-Pyrobot]:**\n\n"
 f"**Python:** `{pyver.split()[0]}`\n"
 f"**Pyrogram:** `{pyrover}`\n"
@@ -96,7 +97,7 @@ def alive(message):
     if CUSTOM_MSG.lower() == 'uncrn':
         uncrn(message)
         return
-    edit(message, logo, f'{UNICORN_ALV}')
+    edit(message, f'{UNICORN_ALV}')
 
 @unicorn(pattern='^.echo')
 def test_echo(message):
